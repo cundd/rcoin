9d9258ff538190556d3168a83f57fbf812801518
Daniel Corn <info@cundd.net>
2017-12-16 21:20:13 +0100

    Move the Rate Series into the Rate module

--- src/rate/mod.rs
+++ src/rate/mod.rs
@@ -1,4 +1,8 @@
-#[derive(Serialize, Deserialize, Debug)]
+mod rate_series;
+
+pub use self::rate_series::RateSeries;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Rate {
     //": "bitcoin",
     pub id: String,
@@ -7,7 +11,7 @@ pub struct Rate {
     //": "BTC",
     pub symbol: String,
     //": "1.0",
-    pub price_btc: String,
+    pub price_btc: f32,
     //": "573.137",
     pub price_usd: f32,
     // "7278.69583695",
@@ -20,7 +24,7 @@ impl Rate {
             id: "bitcoin".to_owned(),
             name: "Bitcoin".to_owned(),
             symbol: "BTC".to_owned(),
-            price_btc: "1.0".to_owned(),
+            price_btc: 1.0,
             price_usd: price_usd.clone(),
             price_eur: price_eur.clone(),
         }
---
+++ src/rate/rate_series.rs
@@ -0,0 +1,54 @@
+use super::Rate;
+
+#[derive(Clone)]
+pub struct RateSeries {
+    max_len: usize,
+    data: Vec<Rate>
+}
+
+impl RateSeries {
+    pub fn new(max_len: usize) -> Self {
+        RateSeries {
+            max_len,
+            data: Vec::with_capacity(max_len)
+        }
+    }
+    pub fn push(&mut self, rate: Rate) {
+        if self.data.len() == self.max_len {
+            self.data.remove(0);
+        }
+        self.data.push(rate);
+    }
+
+    pub fn data(&self) -> &Vec<Rate> {
+        &self.data
+    }
+
+    #[allow(unused)]
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn last(&self) -> Option<&Rate> {
+        self.data.last()
+    }
+    pub fn last_mut(&mut self) -> Option<&mut Rate> { self.data.last_mut() }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn push_test() {
+        let mut rs = RateSeries::new(2);
+        rs.push(Rate::new(0.01, 0.02));
+        rs.push(Rate::new(0.11, 0.12));
+
+        assert_eq!(2, rs.len());
+
+        rs.push(Rate::new(0.21, 0.22));
+        assert_eq!(2, rs.len());
+    }
+}
