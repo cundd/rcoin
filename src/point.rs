01447072a4b68d385e44906247d231b06392279c
Daniel Corn <info@cundd.net>
2018-01-04 00:23:49 +0100

    Make Point available for production again

--- src/point.rs
+++ src/point.rs
@@ -1,68 +1,61 @@
-/// The Point is currently only meant for testing purpose
-#[cfg(test)]
-mod internal {
-    use matrix::PointTrait;
+use matrix::PointTrait;
 
-    #[derive(Debug, Copy, Clone, PartialEq)]
-    pub struct Point {
-        pub x: usize,
-        pub y: usize,
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl Point {
+    #[allow(unused)]
+    pub fn new(x: usize, y: usize) -> Self {
+        Point { x, y }
     }
 
-    impl Point {
-        #[allow(unused)]
-        pub fn new(x: usize, y: usize) -> Self {
-            Point { x, y }
-        }
-
-        #[allow(unused)]
-        pub fn null() -> Self {
-            Point {
-                x: 0,
-                y: 0,
-            }
-        }
-    }
-
-    impl PointTrait for Point {
-        fn x(&self) -> usize {
-            self.x
-        }
-
-        fn y(&self) -> usize {
-            self.y
-        }
-
-        fn with_x(&self, new_x: usize) -> Self {
-            let mut clone = self.clone();
-            clone.x = new_x;
-
-            clone
-        }
-
-        fn with_y(&self, new_y: usize) -> Self {
-            let mut clone = self.clone();
-            clone.y = new_y;
-
-            clone
-        }
-
-        fn with_x_y(&self, new_x: usize, new_y: usize) -> Self {
-            let mut clone = self.clone();
-            clone.x = new_x;
-            clone.y = new_y;
-
-            clone
-        }
-    }
-
-    impl ::std::fmt::Display for Point {
-        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-            let _ = write!(f, "Point {}x{}", self.x, self.y);
-            Ok(())
+    #[allow(unused)]
+    pub fn null() -> Self {
+        Point {
+            x: 0,
+            y: 0,
         }
     }
 }
 
-#[cfg(test)]
-pub use self::internal::Point;
+impl PointTrait for Point {
+    fn x(&self) -> usize {
+        self.x
+    }
+
+    fn y(&self) -> usize {
+        self.y
+    }
+
+    fn with_x(&self, new_x: usize) -> Self {
+        let mut clone = self.clone();
+        clone.x = new_x;
+
+        clone
+    }
+
+    fn with_y(&self, new_y: usize) -> Self {
+        let mut clone = self.clone();
+        clone.y = new_y;
+
+        clone
+    }
+
+    fn with_x_y(&self, new_x: usize, new_y: usize) -> Self {
+        let mut clone = self.clone();
+        clone.x = new_x;
+        clone.y = new_y;
+
+        clone
+    }
+}
+
+impl ::std::fmt::Display for Point {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        let _ = write!(f, "Point {}x{}", self.x, self.y);
+        Ok(())
+    }
+}
